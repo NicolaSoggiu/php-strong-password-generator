bc7d447f9ce919fb35581d65eb5bc6e9b9d988b3
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-06-09 16:19:01 +0200

    fix the path of index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include_once __DIR__ .  "./functions.php";
+include_once __DIR__ .  "/functions.php";
 ?>
 
 
@@ -11,10 +11,8 @@ include_once __DIR__ .  "./functions.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Strong Password Generator</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
     </script>
 </head>
 
@@ -32,8 +30,7 @@ include_once __DIR__ .  "./functions.php";
                 </div>
             </div>
             <div class="p-2 d-flex flex-column align-items-start">
-                <input type="number" class="form-control w-50 mt-3" style="margin-right:150px"
-                    placeholder="Enter a number" id="password" name="password">
+                <input type="number" class="form-control w-50 mt-3" style="margin-right:150px" placeholder="Enter a number" id="password" name="password">
                 <div class="pt-2">
                     <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
                     <label class="form-check-label" for="flexRadioDefault1">
@@ -48,20 +45,20 @@ include_once __DIR__ .  "./functions.php";
                 </div>
                 <div class=" mt-3 d-flex flex-column align-items-start">
                     <div>
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                        <label class="form-check-label" for="flexCheckDefault">
+                        <input class="form-check-input" type="letters" value="" id="letters">
+                        <label class="form-check-label" for="letters" name="chars[]">
                             Letters
                         </label>
                     </div>
                     <div>
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                        <label class="form-check-label" for="flexCheckDefault">
+                        <input class="form-check-input" type="numbers" value="" id="numbers">
+                        <label class="form-check-label" for="numbers" name="chars[]">
                             Numbers
                         </label>
                     </div>
                     <div>
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                        <label class="form-check-label" for="flexCheckDefault">
+                        <input class="form-check-input" type="symbols" value="" id="symbols">
+                        <label class="form-check-label" for="symbols" name="chars[]">
                             Symbols
                         </label>
                     </div>
@@ -69,10 +66,9 @@ include_once __DIR__ .  "./functions.php";
         </form>
 
     </div>
-    <div class="container bg-white text-center d-flex flex-column align-items-center w-75"
-        style="max-width:fit-content">
+    <div class="container bg-white text-center d-flex flex-column align-items-center w-75" style="max-width:fit-content">
         <?= stampPassword($password_length);
-            ?>
+        ?>
     </div>
 </body>
 
